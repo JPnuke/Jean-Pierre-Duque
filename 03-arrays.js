4eb85e0f6213c4665fca42c38f63b84623af3787
Jean Pierre Duque <jpduquedoe@gmail.com>
2020-05-07 11:13:52 -0500

    cambios

---
+++ 03-arrays.js
@@ -0,0 +1,19 @@
+var arregloNumero = [1, 2, 3, 4]
+// console.log(arregloNumero)
+// indexOf, encontra el indice del elemento
+// console.log(arregloNumero.indexOf(0))
+// añadir elementos, Push
+//console.log(arregloNumero.push(10))
+// console.log(arregloNumero)
+// acceder elementos
+// console.log(arregloNumero[2])
+// slice y splice
+
+// slice => dicidir el vector original;
+var arregloSlice = arregloNumero.slice(0, 1)
+//console.log(arregloSlice);
+//console.log(arregloNumero);
+// splice => dicidir el vector original;
+var arregloSplice = arregloNumero.splice(0, 2)
+console.log(arregloSplice);
+console.log(arregloNumero);
