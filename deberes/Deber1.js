edc8d838eeada9055bc8217a0d3b1d450a6c5627
Jean Pierre Duque <jpduquedoe@gmail.com>
2020-05-07 12:28:22 -0500

    deber

---
+++ deberes/Deber1.js
@@ -0,0 +1,21 @@
+/*
+arreglo uno, multiplique * 2, y me filtrar los pares, usar map y filter
+arreglo dos, filter los los impares , sumar + 2
+
+JSON
+{
+    arreglos: {
+        uno: [2, 4, 5, 8, 6, 9, 10],
+        dos: [
+            {valor: 1},
+            {valor: 4},
+            {valor: 2},
+            {valor: 24},
+            {valor: 100},
+            {valor: 23},
+            {valor: 45},
+            {valor: 23}
+        ],
+    }
+
+*/
