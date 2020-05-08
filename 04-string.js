ca47068488360e8c52309ec019570984e2055e1c
Jean Pierre Duque <jpduquedoe@gmail.com>
2020-05-08 11:08:40 -0500

    cambios

---
+++ 04-string.js
@@ -0,0 +1,23 @@
+var nombre = "Jean-Pierre"
+// console.log(nombre.length);
+
+// console.log(nombre.indexOf('i'));
+
+// console.log(nombre.search('i'));
+
+// console.log(nombre.slice(1, 3));
+
+// console.log(nombre.substr(1, 3));
+
+// console.log(nombre.substring(1, 3));
+
+// console.log(nombre.toLowerCase());
+
+// console.log(nombre.toUpperCase());
+
+var texto = "Hola mundo"
+console.log(texto.replace('Hola','Hello'))
+
+console.log(texto.concat(' , ',nombre))
+
+console.log(`${texto} mas mi nombre: ${nombre}`)
