da62acceb75a9bc69883b7666d76c783669a7e0b
Jean Pierre Duque <jpduquedoe@gmail.com>
2020-05-11 11:53:13 -0500

    cambios

---
+++ 07-callbacks.js
@@ -0,0 +1,98 @@
+// JavaScript Document
+
+//API, rest API
+// Programación funcional
+
+// callback función que devuelve otra función
+
+/*function programacionFuncional(numero1, () => { }, l) {
+}*/
+
+
+/*
+function primeraFuncion() {
+    setTimeout(
+        () => {
+            console.log('primera')
+        }, 500
+    )
+
+}
+
+function segundaFuncion() {
+    console.log('2')
+}
+
+//primeraFuncion()
+//segundaFuncion()
+
+function saluda(nombre, callback) {
+    console.log('saludo a', nombre)
+    callback()
+}
+
+function funcionCallback() {
+    primeraFuncion()
+    segundaFuncion()
+    console.log('ya salude')
+}
+
+saluda('Jean', () => {
+console.log('saludaste')
+}) */
+
+/*function saludar(nombre) {
+    console.log(`ejectuo saludo a ${nombre}`)
+    return nombre
+}
+function peticionUsuario(nombre, callback) {
+    callback(nombre)
+}
+peticionUsuario('Jean', saludar)
+peticionUsuario('Jean', (saludar) => {
+    console.log(saludar);
+})*/
+
+/*var arreglo = ['juan', 'juan', 'juan']
+var nombreNuevo = 'juan'
+function anadirNombre(arreglo, nombre, indice, cb) {
+    //arreglo.push(nombre);
+    arreglo[indice] = nombre
+    cb(
+        {
+            mensaje: 'exito',
+            arreglo
+        }
+    )
+}
+anadirNombre(arreglo, 'kevin', 2, (mensaje) => {
+    console.log(mensaje);
+});*/
+
+var arregloUsuario = [1, 2, 3, 4, 5]
+
+function listarUsuario(arreglo, cb) {
+    arreglo
+        .forEach(
+            usuario => {
+                console.log(usuario);
+            });
+    cb({
+        mensaje: 'lista de usuario'
+    })
+}
+
+listarUsuario(arregloUsuario, (respuestaMensaje) => {
+    console.log(respuestaMensaje);
+})
+
+// DEBER
+/*
+usar un arreglo de json (nombre, edad, fechaNacimiento, Telf)
+- callback crear UN USUARIO (JSON) -> buscar en si ya existe debe impirmir q ya existe y sino crearlo
+- callback editar un usuario
+- callback eliminar un usuario
+- callback listar usuario
+*/
+
+
