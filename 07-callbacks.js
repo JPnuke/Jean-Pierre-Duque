6962fee01faf6f50e93832e1769cf2c91b24f2f9
Jean Pierre Duque <jpduquedoe@gmail.com>
2020-05-11 14:21:15 -0500

    cambios

--- 07-callbacks.js
+++ 07-callbacks.js
@@ -86,13 +86,5 @@ listarUsuario(arregloUsuario, (respuestaMensaje) => {
     console.log(respuestaMensaje);
 })
 
-// DEBER
-/*
-usar un arreglo de json (nombre, edad, fechaNacimiento, Telf)
-- callback crear UN USUARIO (JSON) -> buscar en si ya existe debe impirmir q ya existe y sino crearlo
-- callback editar un usuario
-- callback eliminar un usuario
-- callback listar usuario
-*/
 
 
