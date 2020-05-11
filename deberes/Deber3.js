281fa263d22d7f95057d91115dec8014e7b474c4
Jean Pierre Duque <jpduquedoe@gmail.com>
2020-05-11 14:21:28 -0500

    deber

---
+++ deberes/Deber3.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+/*DEBER
+
+usar un arreglo de json (nombre, edad, fechaNacimiento, Telf)
+- callback crear UN USUARIO (JSON) - buscar en si ya existe debe impirmir q ya existe y sino crearlo
+- callback editar un usuario
+- callback eliminar un usuario
+- callback listar usuario */
+
+
