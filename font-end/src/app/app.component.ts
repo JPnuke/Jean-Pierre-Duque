e2a63c5e14f6522b61aa0f7b967395097f2b5b0e
Jean Pierre Duque <jpduquedoe@gmail.com>
2020-05-15 17:02:29 -0500

    Pipes,componentes, css

--- font-end/src/app/app.component.ts
+++ font-end/src/app/app.component.ts
@@ -10,12 +10,12 @@ export class AppComponent {
 
   tituloPadre = 'soy tu padre hola';
   nombreBotonPadre = 'alertar!!!!!!!!';
-  imagenPadre = 'https://as.com/futbol/imagenes/2020/01/27/album/1580109965_448579_1580110002_album_grande.jpg';
+  imagenPadre = 'https://wallpaperaccess.com/full/30642.jpg';
 
   datosHijo = {
     tituloPadre: 'soy tu padre hola',
     nombreBotonPadre: 'alertar!!!!!!!!',
-    imagenPadre: 'https://as.com/futbol/imagenes/2020/01/27/album/1580109965_448579_1580110002_album_grande.jpg',
+    imagenPadre: 'https://wallpaperaccess.com/full/30642.jpg',
   };
 
 
