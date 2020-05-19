c1d7ecf5a83414820af744032542a4da132cdd8a
Jean Pierre Duque <jpduquedoe@gmail.com>
2020-05-18 19:07:11 -0500

    réplica página web

--- proyecto-angular/src/app/app.component.ts
+++ proyecto-angular/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto-angular';
+  esconderHola = false;
+  arregloNumeros = [{ nombre: 'jean' }, { apellido: 'pierre' }];
+  eventoClick() {
+    this.esconderHola = true;
+  }
 }
